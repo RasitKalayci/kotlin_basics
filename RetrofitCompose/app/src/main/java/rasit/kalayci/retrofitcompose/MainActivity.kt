d8233edc9521f7b601eb5784e5dedd8456c63009
Rasit Kalayci <210316075@ogr.cbu.edu.tr>
2026-03-02 15:50:01 +0300

    Add RetrofitCompose Android project

---
+++ RetrofitCompose/app/src/main/java/rasit/kalayci/retrofitcompose/MainActivity.kt
@@ -0,0 +1,140 @@
+package rasit.kalayci.retrofitcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import rasit.kalayci.retrofitcompose.model.CryptoModel
+import rasit.kalayci.retrofitcompose.service.CryptoAPI
+import rasit.kalayci.retrofitcompose.ui.theme.RetrofitComposeTheme
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RetrofitComposeTheme {
+                MainScreen()
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+    var cryptomodels = remember { mutableStateListOf<CryptoModel>() }
+    val BASE_URL ="https://raw.githubusercontent.com/"
+    val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(CryptoAPI::class.java)
+val call = retrofit.getData()
+call.enqueue(object: Callback<List<CryptoModel>> {
+    override fun onResponse(
+        call: Call<List<CryptoModel>?>,
+        response: Response<List<CryptoModel>?>
+    ) {
+       if (response.isSuccessful) {
+           response.body()?.let {
+               cryptomodels.addAll(it)
+           }
+
+       }
+    }
+
+    override fun onFailure(
+        call: Call<List<CryptoModel>?>,
+        t: Throwable
+    ) {
+        t.printStackTrace()
+    }
+
+})
+
+    Scaffold(
+        topBar = { AppBar() }
+    ) { paddingValues ->
+        // İçerik buraya:
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues)) {
+            CryptoList(cryptos = cryptomodels)
+
+        }
+    }
+}
+
+@Composable
+fun CryptoList(cryptos: List<CryptoModel>){
+    LazyColumn(contentPadding = PaddingValues(5.dp)){
+        items(cryptos){crypto ->
+            Text(text = crypto.currency)
+            Text(text = crypto.price)
+
+        }
+
+    }
+}
+@Composable
+fun CryptoRow(crypto: CryptoModel){
+    Column(modifier = Modifier.fillMaxWidth()
+        .background(color = MaterialTheme.colorScheme.surface)
+
+    ){
+        Text(text = crypto.currency,
+            style = MaterialTheme.typography.headlineMedium,
+            modifier = Modifier.padding(2.dp),
+            color = MaterialTheme.colorScheme.primary,
+            fontWeight = FontWeight.Bold)
+        Text(text = crypto.price,
+            style = MaterialTheme.typography.headlineSmall,
+            modifier = Modifier.padding(2.dp))
+    }
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppBar() {
+    TopAppBar(
+        title = {
+            Text(text = "Retrofit Compose", color = Color.Black)
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    RetrofitComposeTheme {
+       CryptoRow(crypto= CryptoModel("BTC","50000"))
+    }
+}
